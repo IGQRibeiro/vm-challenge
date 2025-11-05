aa4da11f9d79f36412fc62841c0237172dc3d997
IGQRibeiro <igqribeiro@gmail.com>
2025-11-05 00:50:15 -0300

    chore: initialize Spring Boot project vm-challenge

---
+++ src/main/java/com/gabriel/vm_challenge/VmChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.vm_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/vm_challenge/VmChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.vm_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
