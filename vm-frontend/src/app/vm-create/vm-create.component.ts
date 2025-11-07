0dcdf99df6a5b742383085235facb7a62554ef18
IGQRibeiro <igqribeiro@gmail.com>
2025-11-06 23:26:03 -0300

    feat(front): add base components (login/dashboard/vm-list/vm-create), services and guard

---
+++ vm-frontend/src/app/vm-create/vm-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vm-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './vm-create.component.html',
+  styleUrl: './vm-create.component.scss'
+})
+export class VmCreateComponent {
+
+}
